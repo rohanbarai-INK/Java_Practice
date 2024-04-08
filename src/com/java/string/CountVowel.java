e7500ee71671241c3f2851dd401771a58d24cf8f
Rohan Barai <77430015+rohanbarai-INK@users.noreply.github.com>
2024-04-08 23:30:27 +0530

    Update CountVowel.java

--- src/com/java/string/CountVowel.java
+++ src/com/java/string/CountVowel.java
@@ -1,20 +1,28 @@
-package com.java.string;
 public class CountVowel {
-public static int countVowels(String st) {
-	int vc=0;
-	String vs="AEIOUaeiou";
-	for (int i = 0; i < st.length(); i++) {
-		char a=st.charAt(i);
-		if (vs.indexOf(a)>0) {
-			vc++;
-		}
-	}
-	return vc;
-}
-public static void main(String[] args) {
-	System.out.println(CountVowel.countVowels("RohanBarai"));
-	String ab="Rohan Barai";
-	System.out.println(ab.replace("R", ""));
-	System.out.println(ab);
-}
+    public static void main(String[] args) {
+        String str = "Rohan Barai";
+        int count = 0;
+        int[] vowelCounts = new int[5]; // Index 0: 'a', Index 1: 'e', Index 2: 'i', Index 3: 'o', Index 4: 'u'
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        
+        // Convert the string to lowercase to make it case-insensitive
+        str = str.toLowerCase();
+        
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            for (int j = 0; j < vowels.length; j++) {
+                if (ch == vowels[j]) {
+                    vowelCounts[j]++;
+                    count++;
+                    break; // Break the inner loop once a vowel is found
+                }
+            }
+        }
+        
+        for (int k = 0; k < vowels.length; k++) {
+            System.out.println("Number of '" + vowels[k] + "' in the string: " + vowelCounts[k]);
+        }
+        
+        System.out.println("Total number of vowels in the string: " + count);
+    }
 }
