6397c60eb8c193dae0339b931c0aa0728c078651
Rohan Barai <77430015+rohanbarai-INK@users.noreply.github.com>
2026-02-02 13:28:35 +0530

    Enhance character counting in CountStringChar
    Updated character counting logic to ignore spaces and handle case insensitivity.

--- src/com/java/string/CountStringChar.java
+++ src/com/java/string/CountStringChar.java
@@ -4,27 +4,31 @@ import java.util.HashSet;
 
 public class CountStringChar {
 
-	public static void main(String[] args) {
-		
-		String name= "Rohan Barai";
-	
-			//crate collection and add character inside it
-		HashSet<Character> set=new HashSet<>();
-		for (int i = 0; i < name.length(); i++) {
-			set.add(name.charAt(i));
-		}
-		//comparing collection with each ch from string
-		for (Character ch1 : set) {
-			int count=0;
-			for (int i = 0; i < name.length(); i++) {
-				if (ch1==name.charAt(i)) {
-					count ++;
-				}
-			}
-			System.out.println(ch1+" "+count);
-		}
-		
-	}
+    public static void main(String[] args) {
+
+        String name = "Rohan Barai";
+
+        // Convert to lowercase for case-insensitive comparison
+        name = name.toLowerCase();
+
+        // Create collection and add characters (excluding space)
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < name.length(); i++) {
+            char ch = name.charAt(i);
+            if (ch != ' ') {        // ignore space
+                set.add(ch);
+            }
+        }
+
+        // Compare each character from set with string
+        for (Character ch1 : set) {
+            int count = 0;
+            for (int i = 0; i < name.length(); i++) {
+                if (ch1 == name.charAt(i)) {
+                    count++;
+                }
+            }
+            System.out.println(ch1 + " " + count);
+        }
+    }
 }
-
-
